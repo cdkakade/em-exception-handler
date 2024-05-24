afc42ca42c1431f9849520ac4deceb732da3c315
cdkakade <cdkakade1@gmail.com>
2024-05-24 15:25:52 +0530

    modified dependencies

--- src/main/java/expense/manager/exception/custom/CustomRuntimeException.java
+++ src/main/java/expense/manager/exception/custom/CustomRuntimeException.java
@@ -2,15 +2,16 @@ package expense.manager.exception.custom;
 
 public class CustomRuntimeException extends RuntimeException {
 
-    public CustomRuntimeException() {
-        super();
-    }
+	public CustomRuntimeException() {
+		super();
+	}
 
-    public CustomRuntimeException(String message) {
-        super(message);
-    }
+	public CustomRuntimeException(String message) {
+		super(message);
+	}
 
-    public CustomRuntimeException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	public CustomRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
 }
--- src/main/java/expense/manager/exception/custom/RecordNotFoundException.java
+++ src/main/java/expense/manager/exception/custom/RecordNotFoundException.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Record Not Found")
 public class RecordNotFoundException extends CustomRuntimeException {
 
-    public RecordNotFoundException() {
-        super();
-    }
+	public RecordNotFoundException() {
+		super();
+	}
 
-    public RecordNotFoundException(String message) {
-        super(message);
-    }
+	public RecordNotFoundException(String message) {
+		super(message);
+	}
+
 }
