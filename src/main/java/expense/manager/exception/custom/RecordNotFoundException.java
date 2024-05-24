61e69e3a2dea67e0c297a6197def08fc86be3027
cdkakade <cdkakade1@gmail.com>
2024-05-24 15:25:52 +0530

    Initial commit

---
+++ src/main/java/expense/manager/exception/custom/RecordNotFoundException.java
@@ -0,0 +1,16 @@
+package expense.manager.exception.custom;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Record Not Found")
+public class RecordNotFoundException extends CustomRuntimeException {
+
+    public RecordNotFoundException() {
+        super();
+    }
+
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/expense/manager/exception/handler/ApiExceptionHandler.java
@@ -0,0 +1,8 @@
+package expense.manager.exception.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+}
